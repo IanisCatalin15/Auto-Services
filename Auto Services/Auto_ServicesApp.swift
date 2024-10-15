5dacab7f4de13d9cbde19a3ec918cb548a2d75aa
Ianis Catalin <ianiscatalin15@gmail.com>
2024-10-15 13:33:18 +0300

    Initial Commit

---
+++ Auto Services/Auto_ServicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auto_ServicesApp.swift
+//  Auto Services
+//
+//  Created by Ianis Catalin on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Auto_ServicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Auto ServicesTests/Auto_ServicesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Auto_ServicesTests.swift
+//  Auto ServicesTests
+//
+//  Created by Ianis Catalin on 15.10.2024.
+//
+
+import Testing
+@testable import Auto_Services
+
+struct Auto_ServicesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
